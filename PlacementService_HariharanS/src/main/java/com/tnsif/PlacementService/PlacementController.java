e75ef76ad627a2e86aa7ca7a0b40dcc8cb340b49
hariharanins <hari14122003@gmail.com>
2024-04-08 01:23:10 +0530

    Updated Placement Service project

--- PlacementService_HariharanS/src/main/java/com/tnsif/PlacementService/PlacementController.java
+++ PlacementService_HariharanS/src/main/java/com/tnsif/PlacementService/PlacementController.java
@@ -1,20 +1,54 @@
 package com.tnsif.PlacementService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.persistence.NoResultException;
+@RestController
 public class PlacementController {
 	@Autowired
    private PlacementService Service;
+	 @GetMapping("/PlacementService")
+	   public List <Placement> list(){
+		   return Service.listAll();
+	   }
 		
 			@PostMapping("/PlacementService")
 			public void add (@RequestBody Placement place) {
-				Service.InsertRecord(place);
-			
+				Service.save(place);
 			
 			}
-		}
+			 @GetMapping("/PlacementService/{id}")
+			public ResponseEntity<Placement> get(@PathVariable Integer id)
+			 {
+				 
+				try {
+					 Placement P = Service.get(id);
+					 return new ResponseEntity<Placement>(P,HttpStatus.OK);
+				}
+				catch(NoResultException e)
+				{
+					return new ResponseEntity<Placement>(HttpStatus.NOT_FOUND);
+				}
+			 }
+			@DeleteMapping("/PlacementService/{id}")
+             public void delete(@PathVariable Integer id)	
+             {
+				Service.delete(id);
+             
+				}
+			 
+			 }
+     
 	   
-   
+
 
--- PlacementService_HariharanS/src/main/java/com/tnsif/PlacementService/PlacementService.java
+++ PlacementService_HariharanS/src/main/java/com/tnsif/PlacementService/PlacementService.java
@@ -2,6 +2,8 @@ package com.tnsif.PlacementService;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +15,28 @@ import jakarta.transaction.Transactional;
 public class PlacementService {
 	@Autowired
 	private PlacementRepository repo;
-	//to insert the record
-	public void InsertRecord(Placement place){
-		 repo.save(place);
-		
+	
+	//get all the record from list
+	public List<Placement> listAll(){
+		return repo.findAll();
 	}
+	//save the record
+	public void save(Placement place) {
+		repo.save(place);
+	}
+	
+	//to call or retrive  a specific record 
+    public Placement get(Integer id) {
+	return repo .findById(id).get();
+    }
+    // Deleting the particular record with id
+    public void delete(Integer id)
+    {
+    	repo.deleteById(id);
+    }
+    
+	
+	
 		
 		
 	
